56ab4ac46754e40d79c1b9fcf222360e6e962226
Blaze <sandevabeykoon123@gmail.com>
2020-11-12 17:06:58 +0530

    added radio addon and lc200

--- server-data/resources/RadioStation/__resource.lua
+++ server-data/resources/RadioStation/__resource.lua
@@ -2,7 +2,7 @@ resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
 
 -- Example custom radios
 supersede_radio "RADIO_02_POP" { url = "https://revolutionradio.ru:8443/live.ogg", volume = 0.2 }
-supersede_radio "RADIO_03_HIPHOP_NEW" { url = "http://stream.radioreklama.bg/nrj.ogg", volume = 0.2 }
+supersede_radio "RADIO_03_HIPHOP_NEW" { url = "http://209.133.216.3:7073/;stream.mp3", volume = 0.2 }
 
 files {
 	"index.html"
